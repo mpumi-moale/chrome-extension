38c4a37eb729e2ffd6992d21f4aedb9eb00d99b4
Mpumelelo Moale <moalempumi5@gmail.com>
2026-03-04 11:30:26 +0200

    updated code structure

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+// fetching data from localStorage
 // localStorage.clear()
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
+// checking if data in localStorage exist
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     renderLeads()
