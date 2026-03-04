e7de96f7de1abfe6ed26be6d0148d057e3a257b7
Mpumelelo Moale <moalempumi5@gmail.com>
2026-03-04 19:06:21 +0200

    added a save tab button and its function

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
+
 // fetching data from localStorage
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
 
 // checking if data in localStorage exist
 if (leadsFromLocalStorage) {
@@ -12,11 +15,28 @@ if (leadsFromLocalStorage) {
     render(myLeads)
 }
 
+const tabs = [
+    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+]
+
+// save tab button
+tabBtn.addEventListener("click", function() {
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
+
 //  Show saved leads
 function render(leads) {
     let listItems = ""
     for (let i = 0; i < leads.length; i++) {
-        listItems += "<li>" + leads[i] + "</li>"
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
     }
     ulEl.innerHTML = listItems  
 }
